f28a778e4722de8057be685a1635e84de0acad6f
mtoledo <mmts048@gmail.com>
2017-01-15 16:33:45 -0400

    Show a list of Marvel Characters

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,53 @@
 'use strict';
 
 angular.module('myApp.view1', ['pascalprecht.translate', 'sn.skrollr'])
-
   .controller('View1Ctrl', ['$http', '$scope','$anchorScroll','$location', '$window',
     function($http, $scope, $anchorScroll, $location, $window) {
+      $scope.offset = 0;
+      $scope.message = '';
+      $scope.currentPage = 1;
+      $scope.totalCharacters = 1485;
+      $scope.totalPages = Math.floor($scope.totalCharacters/10) + 1;
+      $scope.getAll = "https://gateway.marvel.com:443/v1/public/characters?limit=10&offset="+$scope.offset+"&apikey=25edc4d33b05ade4e5234c3f22ede54b"
 
-      $scope.goToSection = function(section){
-        angular.element('html, body').animate({scrollTop:angular.element('#' + section).position().top}, 1000);
-        //reset to old to keep any additional routing logic from kicking in
-      };
+      angular.element(document).ready(function(){
+        if ($scope.currentPage == 1) {
+          angular.element(document.querySelector('#btn-previus')).addClass("inactive");
+        } else {
+          angular.element(document.querySelector('#btn-previus')).removeClass("inactive");
+        }
+      });
+
+      $http.get($scope.getAll)
+        .success(function(data){
+          $scope.response = data;
+        })
+        .error(function(){
+          $scope.message = "An error has occurred, please try again!";
+        });
+
+      // Pagination
+      $scope.changePage = function(pageNumber) {
+        console.log(pageNumber);
+        if (pageNumber == 1) {
+          $scope.offset = 0;
+          $scope.currentPage = 1;
+          $scope.getAll = "https://gateway.marvel.com:443/v1/public/characters?limit=10&offset="+$scope.offset+"&apikey=25edc4d33b05ade4e5234c3f22ede54b"
+        } else if (pageNumber > 1 && pageNumber < ($scope.totalPages + 1)) {
+          $scope.offset = (10 * (pageNumber - 1)) + 1;
+          $scope.currentPage = pageNumber;
+          $scope.getAll = "https://gateway.marvel.com:443/v1/public/characters?limit=10&offset="+$scope.offset+"&apikey=25edc4d33b05ade4e5234c3f22ede54b"
+        }
+
+        $http.get($scope.getAll)
+          .success(function(data){
+            $scope.response = data;
+          })
+          .error(function(){
+            $scope.message = "An error has occurred, please try again!";
+          });
+      }
+
 
     }
   ]);
